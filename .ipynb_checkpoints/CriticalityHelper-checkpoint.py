e7fa888d50e261822c4cf4a807e29cbf11af4327
Eitan Vilker <evilker@bu.edu>
2025-10-15 19:43:57 -0400

    Removed poorly designed comparisons, added distance and differential expression metrics

--- .ipynb_checkpoints/CriticalityHelper-checkpoint.py
+++ .ipynb_checkpoints/CriticalityHelper-checkpoint.py
@@ -4,8 +4,11 @@ import pandas as pd
 import math
 from scipy import signal
 import scipy.cluster.hierarchy as spc
+from scipy.spatial.distance import cdist
+from sklearn.decomposition import PCA
 import scanpy as sc
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def filterDFByTime(df, timeName, timeValues):
     return df.loc[:, timeValues == timeName]
@@ -21,6 +24,12 @@ def filterDFByGenes(df, genes, negative=False):
 
 def filterDFByGeneExpression(df, gene, threshold=1):
     return df.loc[:, df.loc[gene, :] > threshold]
+
+
+def filterDFByMinimumGeneExpression(df, includeCriteria=None, threshold=0):
+    if includeCriteria is not None:
+        df = df.loc[:, includeCriteria]
+    return df.loc[[val > threshold for val in df.mean(axis=1)], :]
 
 
 def filterDFByGeneVariance(df, threshold=0.01):
@@ -230,15 +239,86 @@ def getGeneEntropy(df, gene, timeName=None, timeValues=None):
     return -1 * totalEntropy
 
 
-def getDifferentiallyExpressedGenes(annObject, differentialColumn, case):
-    annObjectCopy = annObject
+def getDifferentiallyExpressedGenes(annObject, differentialColumn, case, individualCompare=False, useRaw=False, includeCriteria=None):
+    annObjectCopy = annObject.copy()
+    annObjectCopy = annObjectCopy if includeCriteria is None else annObjectCopy[includeCriteria]
+    annotations = annObjectCopy.obs[differentialColumn]
+    annotations = annotations.cat.remove_unused_categories()
+    print("Normalizing...")
     sc.pp.normalize_total(annObjectCopy, inplace=True)
+    # annObjectCopy = sc.pp.normalize_total(annObjectCopy, inplace=False)
     sc.pp.log1p(annObjectCopy, copy=False)
-    sc.tl.rank_genes_groups(annObjectCopy, differentialColumn, method='t-test', use_raw=False, copy=False)
-    diffTable = sc.get.rank_genes_groups_df(annObjectCopy, group=case)
-    reducedDiff = diffTable.loc[np.logical_and(diffTable['pvals_adj'] < 0.05, abs(diffTable['logfoldchanges']) > 2), :]
+    # annObjectCopy = sc.pp.log1p(annObjectCopy, copy=True)
+    
+    if individualCompare:
+        diffTableMap = {}
+        for comparisonGroup in annotations.cat.categories:
+            print("Currently comparing against " + comparisonGroup)
+            if comparisonGroup != case:
+                filteredAnnObject = annObjectCopy[annotations.isin([case, comparisonGroup])].copy()
+                sc.tl.rank_genes_groups(filteredAnnObject, differentialColumn, method='wilcoxon', use_raw=useRaw, copy=False)
+                diffTableMap[comparisonGroup] = sc.get.rank_genes_groups_df(filteredAnnObject, group=case).set_index("names")
+        print("Done!")
+        return diffTableMap
+
+    sc.tl.rank_genes_groups(annObjectCopy, differentialColumn, method='wilcoxon', use_raw=useRaw, copy=False)
+    diffTable = sc.get.rank_genes_groups_df(annObjectCopy, group=case).set_index("names")
+    # reducedDiff = diffTable.loc[np.logical_and(diffTable['pvals_adj'] < 0.05, abs(diffTable['logfoldchanges']) > 2), :]
     print("Complete")
-    return reducedDiff['names']
+    return diffTable, annObjectCopy
+
+
+# Given a DEG table, filter table to genes meeting certain conditions
+def getTopGenes(diffTable, minimumFoldChange=2, outFile=None, surfaceGeneFile="/restricted/projectnb/crem-trainees/Kotton_Lab/Eitan/Transdifferentiation/CellSurfaceGenes.txt", checkSurface=False, requireOverexpression=False):
+    
+    # Set conditions for differential expression
+    logFoldChanges = diffTable['logfoldchanges'] if requireOverexpression else diffTable['logfoldchanges'].abs()
+    conditions = [diffTable['pvals_adj'] < 0.05, logFoldChanges > minimumFoldChange]
+
+    # Set condition for surface genes
+    if checkSurface:
+        cellSurfaceGenes = pd.read_csv(surfaceGeneFile)
+        conditions.append(diffTable.index.isin(cellSurfaceGenes.columns))
+
+    # Apply all conditions
+    combinedCondition = conditions[0]
+    for i in range(1, len(conditions)):
+        combinedCondition = np.logical_and(combinedCondition, conditions[i])
+    filteredGenes = diffTable.loc[combinedCondition, :]
+
+    if outFile is not None:
+        filteredGenes.to_csv(outFile)
+    return filteredGenes
+
+
+# Given DEG between a group and each other, get table of genes with high expression and universal high differential expression
+def getCombinedTopGenes(diffTableMap, df, includeCriteria=None, expressionThreshold=1, missesAllowed=0, outFile=None, checkSurface=False, requireOverexpression=False):
+    if includeCriteria is not None: # Usually filter to the case
+        df = df.loc[:, includeCriteria]
+
+    allGenes = pd.DataFrame(index=df.index)
+    comparisonGroups = list(diffTableMap.keys())
+    for group in comparisonGroups:
+        validGenes = getTopGenes(diffTableMap[group], checkSurface=checkSurface, requireOverexpression=requireOverexpression).index
+        valid = [int(val in validGenes) for val in df.index]
+        allGenes[group] = valid
+
+    # allGenes.set_index("Gene", inplace=True)
+    totalValid = allGenes.sum(axis=1)
+    allGenes["Successes"] = totalValid
+
+    filteredGenes = df.loc[allGenes["Successes"] > len(comparisonGroups) - missesAllowed - 1, :]
+    filteredGenes = filterDFByMinimumGeneExpression(filteredGenes, threshold=expressionThreshold)
+    targetDF = pd.DataFrame({"Average Normalized Expression": filteredGenes.mean(axis=1)}, index=filteredGenes.index)
+    for group in comparisonGroups:
+        DEG = diffTableMap[group].loc[:, "logfoldchanges"].copy().rename("logfoldchanges " + group)
+        targetDF = targetDF.join(DEG, how="left", rsuffix=group)
+
+    if outFile is not None:
+        targetDF.to_csv(outFile)
+    if missesAllowed > 0:
+        targetDF = targetDF.join(allGenes["Successes"], how="left")
+    return targetDF
 
 
 def clusterGenesByCorrelation(df):
@@ -314,4 +394,111 @@ def findDNB(annObject, timeColumnName, timesSorted, differentialColumn, case, co
     rankedGroups = getDominantGroups(df, clustersList, metadata[timeColumnName], timesSorted, differentialColumn, case, control)
     print("Done!")
     return rankedGroups
+
+
+# Get distance of each point to mean of cluster 
+def getClusterDistances(df):
+    mean = df.mean(axis=1)
+    distances = np.linalg.norm(df.values - np.tile(mean, (len(df.columns), 1)).T, axis=0)
+    return distances, mean
+
+
+# Filter dataframe of n-dimensional points down to x % of data closest to the mean 
+def getQuantileData(df, includeCriteria=None, quantile=0.8, quantileMin=None, minSamples=50):
+    if includeCriteria is not None:
+        df = df.loc[:, includeCriteria]
+    if len(df.columns) < minSamples:
+        return None
+    distances, mean = getClusterDistances(df)
+    upperQuantile = np.quantile(distances, quantile)
+    lowerQuantile = 0 if quantileMin is None else np.quantile(distances, quantileMin)
+    return df.loc[:, np.logical_and(distances < upperQuantile, distances > lowerQuantile)]
+
+
+# Find shortest distance between n-dimensional points
+def getMinimumDistance(df1, df2, metric='euclidean'):
+    distances = cdist(df1.values.T, df2.values.T, metric=metric)
+    return np.min(distances), np.unravel_index(np.argmin(distances), distances.shape)
+
+
+# Get mean distance between two clusters
+def getMeanDistance(df1, df2):
+    mean1 = df1.mean(axis=1)
+    mean2 = df2.mean(axis=1)
+    return (np.linalg.norm(mean1 - mean2), (0, 0))
+
+
+# Get DataFrame of minimal distances between clusters given dict of cluster names to points
+def getDistanceMap(clusterMap, method="min"):
+    # Select distance metric
+    if method == "min":
+        distanceFunc = getMinimumDistance
+    elif method == "mean":
+        distanceFunc = getMeanDistance
+    else:
+        print("Invalid distance method selected!")
+        return None
+
+    # Initialize dict of distances between states
+    stateDistancesMap = {}
+    stateList = sorted(clusterMap.keys())
+    stateCount = len(stateList)
+    for state in stateList:
+        stateDistancesMap[state] = {}
+        stateDistancesMap[state][state] = 0
+
+    # Set distance between each pair of clusters
+    for i in range(0, stateCount - 1):
+        state1 = stateList[i]
+        for j in range(i + 1, stateCount):
+            state2 = stateList[j]
+            stateDistancesMap[state1][state2] = stateDistancesMap[state2][state1] = distanceFunc(clusterMap[state1], clusterMap[state2])[0]
+    distances = pd.DataFrame.from_dict(stateDistancesMap, orient="index")
+    return distances
+
+
+# Using state clusters filtered by quantiles, plot matrix of distances between clusters
+def stateDistancePlot(topObject, projectionName, includeCriteria=None, quantile=0.8, quantileMin=None, minSamples=50, 
+                      figX=8, figY=8, title="", outFile=None, method="min"):
+
+    # Get quantiles
+    stateCloseValuesMap = {}
+    projection = topObject.projections[projectionName] if includeCriteria is None else topObject.projections[projectionName].loc[:, includeCriteria]
+    for state in topObject.sortedCellTypes:
+        reducedState = getQuantileData(projection, quantile=quantile, quantileMin=quantileMin, minSamples=minSamples, includeCriteria=topObject.annotations == state)
+        if reducedState is not None:
+            stateCloseValuesMap[state] = reducedState
+
+    # Get distances
+    distances = getDistanceMap(stateCloseValuesMap, method=method)
+    
+    # Plot results
+    plt.subplots(1, 1, figsize=(figX, figY))
+    labels = sorted(distances.keys())
+    sns.heatmap(distances, annot=True, fmt=".2f", cmap='plasma', xticklabels=labels, yticklabels=labels,
+        annot_kws={"size": 10}, cbar=True)
+    plt.xticks(rotation=90)
+    plt.title(title)
+    plt.tight_layout()
+    if outFile is not None:
+        plt.savefig(outFile)
+    plt.show()
+
+
+# Plot average distance to centroid of each cluster
+def selfMeanDistancePlot(clusterMap):
+    stateList = sorted(clusterMap.keys())
+    meanDistances = []
+    for state in stateList:
+        distances = getClusterDistances(clusterMap[state])[0]
+        meanDistances.append(sum(distances) / len(distances))
+    plt.bar(stateList, meanDistances)
+    plt.xticks(rotation=90)
+    plt.show()
+
+
+def getPCA(df):
+    pca = PCA(100)
+    return pca.fit_transform(df)
+
 
